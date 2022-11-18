4fcb1a84866bc6034a632a98d8c4959c941e8bb0
Michele Sorbo <michele.sorbo@gmail.com>
2022-11-18 17:36:21 +0100

    init

--- src/Esercizi/ContoCorrente.java
+++ src/Esercizi/ContoCorrente.java
@@ -15,4 +15,71 @@ public class ContoCorrente {
         5) Info Conto (Restituisce Proprietario e N° Conto)
         0) Esci
      */
+
+    private int saldo;
+    private String proprietario;
+    private String n_conto;
+    String[] movimenti = new String[5];
+    private int conta_movimenti = 0;
+    //private ArrayList<String> movimenti_cc = new ArrayList<String>();
+
+    ContoCorrente(int saldo_iniziale, String proprietario, String conto){
+        this.saldo = saldo_iniziale;
+        this.proprietario = proprietario;
+        this.n_conto = conto;
+    }
+
+    void setProprietatio(String nome) {
+        proprietario = nome;
+    }
+
+    void versa(int somma) {
+        saldo = saldo + somma;
+        System.out.println("Versamento effettuato.");
+        this.movimentiConto("Vesato " + somma);
+    }
+
+    void preleva(int somma) {
+        if(somma > saldo) {
+            System.out.println("Non puoi prelevare saldo inferiore");
+        }else {
+            saldo = saldo - somma;
+            System.out.println("Prelievo effettuato.");
+            this.movimentiConto("Prelevato " + somma);
+        }
+    }
+
+    void stampa_saldo() {
+        System.out.println("Il saldo attuale è: " + saldo);
+    }
+
+    void info_conto() {
+        System.out.println("Proprietario: " + proprietario);
+        System.out.println("N° Conto: " + n_conto);
+        System.out.println("Il saldo attuale è: " + saldo);
+        System.out.println("Hai effettuato " + conta_movimenti + " movimenti sul conto");
+    }
+
+    //Metodo che aggiunge i movimenti nell'array movimenti classico
+	void movimentiConto(String movimento) {
+		int i;
+
+		if(conta_movimenti > 4) {
+			for(i = 1; i < movimenti.length; i++) {
+				movimenti[i-1] = movimenti[i]; //Sposto gli elementi di un indice
+			}
+			movimenti[i-1] = movimento; //Mette il versamento nell'ultima posizione dell'array
+		}else{
+			movimenti[conta_movimenti] = movimento;
+		}
+
+		conta_movimenti++;//Incremento il contatore di movimenti
+	}
+
+    //Metodo che stampa a video il contenuto dell'array movimenti
+	void stampa_movimenti() {
+		for(String el:movimenti) {
+			System.out.println(el);
+		}
+	}
 }
