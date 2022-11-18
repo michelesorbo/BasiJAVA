03c2cacc6c24e71bd2fb9c71186ae2a3687e0538
Michele Sorbo <michele.sorbo@gmail.com>
2022-11-18 10:06:02 +0100

    init

---
+++ src/Esercizi/ConvertitoreVoti.java
@@ -0,0 +1,25 @@
+package Esercizi;
+
+public class ConvertitoreVoti {
+
+    /*
+    Scrivere un metodo per restituire una votazione scritta da un voto numerico.
+    Se il professore inserisce un voto inferiore a 18 il metodo restituisce a video "Bocciato"
+    Se inserisce un voto tra 18 e 20 restituisce "Appena sufficiente"
+    Se inserisce un voto tra 21 e 24 restituisce "Sufficiente"
+    Se inserisce un voto tra 25 e 27 restituisce "Buono"
+    Se inserisce un voto tra 28 e 29 restituisce "Ottimo"
+    Se inserisce un voto con 30 restituisce "Più che ottimo" e deve chiedere se applicare la lode
+    se applica la lode restiruire "Ottimo 30 e lode"
+    Non devo accettare voti superiori a 30
+
+    Il convertitore si ferma con NO ES:
+    Convertitore Voti
+    Vuoi convertire un voto? SI/NO
+     - Inserisci il voto da convertire: 28
+     - Ottimo
+     Vuoi convertire un voto? SI/NO
+      - NO
+      Grazie alla prossima
+     */
+}
