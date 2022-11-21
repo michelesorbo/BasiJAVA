b3dcdb1d53167b719a37f421577ed4eec1f34a94
Michele Sorbo <michele.sorbo@gmail.com>
2022-11-21 09:32:17 +0100

    init

--- src/Base/WrapperclassArrayList.java
+++ src/Base/WrapperclassArrayList.java
@@ -1,6 +1,8 @@
 package Base;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class WrapperclassArrayList {
 
@@ -27,7 +29,7 @@ public class WrapperclassArrayList {
         System.out.println(alunni.size()); //Corrisponde a length di array
 
         //Per visualizzare un elemento dell'ArrayList uso il metodo get(indice dell'elemento)
-        System.out.println(alunni.get(0)); //Corrisponde alunni
+        System.out.println(alunni.get(0)); //Corrisponde alunni[i]
 
 
         //Stampo tutti gli elementi dell'ArrayList
@@ -36,9 +38,21 @@ public class WrapperclassArrayList {
             System.out.println(alunni.get(i));
         }
 
+
         //Modificare elementi in ArrayList
         alunni.set(1, "beatrice"); //alunni[1] = "beatrice"
         System.out.println(alunni.get(1));
+
+        //Stampo l'Arraylist ordinato
+        System.out.println("Stampo array ordinato");
+        Collections.sort(alunni); //Ordino gli elemnti dell'array la modifica è permanente
+        //Collections.reverse(alunni); //Ordina in modo inverso gli elementi dell'array
+        for (int i = 0; i < alunni.size(); i++) {
+            System.out.println(alunni.get(i));
+        }
+
+        //Per conoscere l'indice di un elemnto dell'ArrayList
+        System.out.println(alunni.indexOf("rossella"));
 
         //Elimenare un elemento dall'ArrayList
         alunni.remove(1);
