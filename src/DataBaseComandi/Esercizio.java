1d16acb37f5dcb732a33d3c5291cca209b0e8558
Michele Sorbo <michele.sorbo@gmail.com>
2022-11-28 12:14:37 +0100

    init

--- src/DataBaseComandi/Esercizio.java
+++ src/DataBaseComandi/Esercizio.java
@@ -1,7 +1,12 @@
 package DataBaseComandi;
 
 /*
-Creare una queri di inserimento di una nuova squadra inserirla solo se il nome della squadra non è presente nel DB
+Creare un programma che chieda quale operazione si vuole compiere:
+1) Visualizza tutte le squadre
+2)Chiedere quale le squadre di quale serie visulizzare
+3)Inserire una nuova squadra e controllare se il nome della squadra è già presente nel DB
+4)Chiedere quale squadra modificare e inserire i campi modificati nel DB
+
  */
 public class Esercizio {
 }
