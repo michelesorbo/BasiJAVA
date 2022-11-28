bd52397304f474835d3351f448e7f9d12dc1a13f
Michele Sorbo <michele.sorbo@gmail.com>
2022-11-28 11:01:05 +0100

    init

--- src/DataBaseComandi/InsertDB.java
+++ src/DataBaseComandi/InsertDB.java
@@ -4,9 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class InsertDB {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String classe;
+        String piano;
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/basijava", "root", "root");
             Statement st = connection.createStatement();
@@ -14,8 +18,16 @@ public class InsertDB {
             /*
             INSERT INTO table_name (column1, column2, column3, ... -- OPZIONALI SE DIAMO TUTTI I CAMPI DELLA TABELLA NELL'ORDINE ESATTO--)
             VALUES (value1, value2, value3, ...);
+
+            RICORDA i valori colonna vanno inseriti  senza '
              */
-            String query = "INSERT INTO classi VALUES('1D', '2° Piano')";
+            System.out.println("Inserisci il nome della classe: ");
+            classe = in.nextLine();
+            System.out.println("Inserisci il piano");
+            piano = in.nextLine();
+            String query = "INSERT INTO classi (nome, piano) VALUES('"+classe+"', '"+piano+"')";
+            System.out.println(query);
+            //String query = "INSERT INTO classi (nome) VALUES('2D')";
 
             st.executeUpdate(query);
 
